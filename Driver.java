2c02534b7c60f5f54a50e3787552ded960485139
Zijun Deng <jundeng@ucdavis.edu>
2019-08-23 19:25:51 -0700

    frame

--- Driver.java
+++ Driver.java
@@ -1,1 +1,20 @@
+import java.io.IOException;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.db.DBConfiguration;
+import org.apache.hadoop.mapreduce.lib.db.DBOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class Driver{
+	public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException {
+		//MR1
+
+		//MR2
+	}
+}
